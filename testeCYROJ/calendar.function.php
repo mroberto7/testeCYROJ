2469d9d4fe34e467aa7571ae6fce0c25addeb37b
Marcos Costa <60942995+mroberto7@users.noreply.github.com>
2022-07-28 15:28:12 -0300

    cagando o calendario

--- testeCYROJ/calendar.function.php
+++ testeCYROJ/calendar.function.php
@@ -1,20 +1,4 @@
-// register custom post type to work with
- function lc_create_post_type() {
- // set up labels
- $labels = array (
- 'name' => 'Events',
- 'singular_name' => 'Event',
- 'add_new' => 'Add New Event',
- 'add_new_item' => 'Add New Event',
- 'edit_item' => 'Edit Event',
- 'new_item' => 'New Event',
- 'all_items' => 'All Events',
- 'view_item' => 'View Event',
- 'search_items' => 'Search Events',
- 'not_found' => 'No Events Found',
- 'not_found_in_trash' => 'No Events found in Trash',
- 'parent_item_colon' => '',
- 'menu_name' => 'Events',
+
  );
  //register post type
  register_post_type ( 'event', array(
